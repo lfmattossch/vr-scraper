e70e8175c48560566944fed633e17480d1518e1e
Luiz Fernando Mattos Schlindwein <l.f.mattos.sch@gmail.com>
2016-08-05 04:50:23 -0300

    adding a view in order to show the database results, unfortunately not with an API as in the description of challenge

--- scrapper/views.py
+++ scrapper/views.py
@@ -1,5 +1,15 @@
 from django.http import HttpResponse
+from django.template import loader
+
+from scrapper.models import Article
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the scraper index.")
+    latest_article_list = Article.objects.order_by('-publication_date')[:10]
+    template = loader.get_template('scrapper/template.html')
+    context = {
+        'articles': latest_article_list.values(
+        	'title', 'publication_date', 'author__name', 'content'
+        )
+    }
+    return HttpResponse(template.render(context, request))
